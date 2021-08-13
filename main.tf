80805920147c1b8bdd01b8b91bc6c66fa05702d9
Sean Sundberg <seansund@us.ibm.com>
2021-08-13 00:24:39 -0500

    Updates logic to use igc cli
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
+  bin_dir = "${path.cwd}/bin"
   layer = "services"
-  config_project = var.config_projects[local.layer]
-  application_branch = "main"
-  config_namespace = "default"
   ingress_host = "dashboard-${var.namespace}.${var.cluster_ingress_hostname}"
   endpoint_url = "http${var.tls_secret_name != "" ? "s" : ""}://${local.ingress_host}"
+  yaml_dir = "${path.cwd}/.tmp/dashboard"
+  name = "dashboard"
   global = {
     ingressSubdomain = var.cluster_ingress_hostname
     clusterType = var.cluster_type
@@ -30,25 +30,40 @@ locals {
     developer-dashboard = local.dashboard_config
     tool-config = local.tool_config
   })
+  values_file = "values-${var.serverName}.yaml"
 }
 
-resource null_resource setup_application {
+resource null_resource setup_binaries {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-application.sh '${var.application_repo}' '${var.application_paths[local.layer]}' '${var.namespace}' '${local.values_content}'"
+    command = "${path.module}/scripts/setup-binaries.sh"
 
     environment = {
-      TOKEN = var.application_token
+      BIN_DIR = local.bin_dir
     }
   }
 }
 
-resource null_resource setup_argocd {
-  depends_on = [null_resource.setup_application]
+resource null_resource create_yaml {
+  depends_on = [null_resource.setup_binaries]
+
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-argocd.sh '${var.config_repo}' '${var.config_paths[local.layer]}' '${local.config_project}' '${var.application_repo}' '${var.application_paths[local.layer]}/dashboard' '${var.namespace}' '${local.application_branch}'"
+    command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${local.values_file}'"
 
     environment = {
-      TOKEN = var.config_token
+      VALUES_CONTENT = local.values_content
+    }
+  }
+}
+
+resource null_resource setup_gitops {
+  depends_on = [null_resource.create_yaml]
+
+  provisioner "local-exec" {
+    command = "$(command -v igc || command -v ${local.bin_dir}/igc) gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.serverName}' -l '${local.layer}' --valueFiles 'values.yaml,${local.values_file}'"
+
+    environment = {
+      GIT_CREDENTIALS = yamlencode(var.git_credentials)
+      GITOPS_CONFIG   = yamlencode(var.gitops_config)
     }
   }
 }

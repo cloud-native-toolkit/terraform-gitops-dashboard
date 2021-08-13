8d4eb7b6002c4210cc337e3240e936cb7b704dc8
Sean Sundberg <seansund@us.ibm.com>
2021-08-13 00:28:37 -0500

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-dashboard.tf
+++ test/stages/stage2-dashboard.tf
@@ -1,13 +1,8 @@
 module "gitops_dashboard" {
   source = "./module"
 
-  config_repo = module.gitops.config_repo
-  config_token = module.gitops.config_token
-  config_paths = module.gitops.config_paths
-  config_projects = module.gitops.config_projects
-  application_repo = module.gitops.application_repo
-  application_token = module.gitops.application_token
-  application_paths = module.gitops.application_paths
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
   namespace = module.gitops_namespace.name
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
   cluster_type             = module.dev_cluster.platform.type_code

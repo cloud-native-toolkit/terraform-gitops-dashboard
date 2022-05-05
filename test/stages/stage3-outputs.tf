d7d4f8420e097cd2081b44369f022043d212928d
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-05-05 16:55:12 +0530

    Template changes
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_sonarqube.name
-        branch      = module.gitops_sonarqube.branch
-        namespace   = module.gitops_sonarqube.namespace
-        server_name = module.gitops_sonarqube.server_name
-        layer       = module.gitops_sonarqube.layer
-        layer_dir   = module.gitops_sonarqube.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_sonarqube.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_sonarqube.type
+        name        = module.gitops_dashboard.name
+        branch      = module.gitops_dashboard.branch
+        namespace   = module.gitops_dashboard.namespace
+        server_name = module.gitops_dashboard.server_name
+        layer       = module.gitops_dashboard.layer
+        layer_dir   = module.gitops_dashboard.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_dashboard.layer == "services" ? "2-services" : "3-applications")
+        type        = module.gitops_dashboard.type
       })
     }
   }

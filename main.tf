dbe2d297d4a84ef0b572df6f01d1586f274bce9b
Sean Sundberg <seansund@us.ibm.com>
2022-09-04 23:00:43 -0500

    Changes to use gitops provider
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
   endpoint_url = "http${var.tls_secret_name != "" ? "s" : ""}://${local.ingress_host}"
   yaml_dir = "${path.cwd}/.tmp/dashboard"
   name = "dashboard"
+  type = "base"
   tmp_dir = "${path.cwd}/.tmp/dashboard"
   application_branch = "main"
   global = {
@@ -39,62 +40,17 @@ resource null_resource create_yaml {
   }
 }
 
-/*resource null_resource create_yaml {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${local.values_file}'"
-    environment = {
-      VALUES_CONTENT = ""
-      VALUES_SERVER_CONTENT = yamlencode(local.values_server_content)
-      TMP_DIR = local.tmp_dir
-    }
-  }
-}*/
 
-/*resource null_resource setup_gitops {
+resource gitops_module module {
   depends_on = [null_resource.create_yaml]
 
-  provisioner "local-exec" {
-    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --valueFiles 'values.yaml,${local.values_file}' --debug"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(yamlencode(var.git_credentials))
-      GITOPS_CONFIG   = yamlencode(var.gitops_config)
-    }
-  }
-}*/
-
-resource null_resource setup_gitops {
-  depends_on = [null_resource.create_yaml]
-
-  triggers = {
-    name = local.name
-    namespace = var.namespace
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = "base"
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}' --cascadingDelete=false"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+  name = local.name
+  namespace = var.namespace
+  content_dir = local.yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
-
